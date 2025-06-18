d023c32704b999f519878b15591c30dd2dc7c164
jp50g <joao.schienemeier.neves@escola.pr.gov.br>
2025-06-18 12:05:34 -0300

    Add files via upload

---
+++ neves2B/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Simpsons',
+    'Quanto tempo existe a franquia Simpsons?',
+    'A franquia "Os Simpsons" existe há 37 anos, tendo começado como curtas de animação no programa "The Tracey Ullman Show" em 1987.'
+)
+
+criaCartao(
+    'Simpsons',
+    'Quantas episodios e temporadas tem o Simpsons?',
+    'A série "Os Simpsons" tem atualmente 36 temporadas e 790 episódios'
+)
+
+criaCartao(
+    'Champions',
+    'Quantas Champions tem o real madrid',
+    'Ao longo de mais 60 edições, o Real Madrid venceu a competição 15 vezes'
+)
+
+criaCartao(
+    'Champions',
+    'Quem foi o ultimo campião da Champions?',
+    'O PSG ganhou a ultima Champions'
+)
